067c059e63d041cfbd86cd9292934b007036abba
hawlermathew2007 <hawlermathewgit@gmail.com>
2024-10-14 23:29:10 +0800

    Meme Image is Used

---
+++ data.py
@@ -0,0 +1,37 @@
+# Just Simply the Gap between the dino and the window side to make it look natural
+window_gap = 50
+
+# About Dino Path
+type_of_Dino = 'Blue'
+dino_path = f'sprites/{type_of_Dino}/dino.png'
+idle_path = f'sprites/{type_of_Dino}/Idle'
+walking_path = f'sprites/{type_of_Dino}/Walking'
+running_path = f'sprites/{type_of_Dino}/Running'
+
+# Meme path
+imagesMeme_path = 'meme/images'
+gifsMeme_path = 'meme/gifs'
+
+# Sprite
+duplicated_sprites = []
+dino_is_flipped = False
+
+idle_index = 0
+idle_time = 600
+idle_sprites = [f'{idle_path}/idle1.png',f'{idle_path}/idle2.png',f'{idle_path}/idle3.png',f'{idle_path}/idle4.png']
+startOf_idleDuplication = True
+
+walking_index = 0
+walking_sprites = [f'{walking_path}/walking1.png',f'{walking_path}/walking2.png',f'{walking_path}/walking3.png',f'{walking_path}/walking4.png',
+					f'{walking_path}/walking5.png',f'{walking_path}/walking6.png']
+
+running_index = 0
+running_sprites = [f'{running_path}/running1.png',f'{running_path}/running2.png',f'{running_path}/running3.png',f'{running_path}/running4.png',
+					f'{running_path}/running5.png',f'{running_path}/running6.png', f'{running_path}/running7.png']
+
+# Velocity of the Dino
+walking_speed = 50 # in millisecond
+running_speed = 25
+walking_step = 2
+running_step = 4
+
--- funny.py
+++ funny.py
@@ -5,55 +5,38 @@ import random
 import threading
 import os
 import time
-
-# Note: must 4 directions, separate to 2 groups: West & East and South & North || Create Destination (!= current dino coordination); 
-# Must Reckon Limitation First (with the prerequisite: 300 pixels away dino and 500 pixels move maximum 
-# -> Must go to situation that have to determine the directions first bcuz
-# the dino may be close to the computer screen side)
-# Stage 1: Must Locate the Dino first -> Decide the direction || random the direction via Limitation and Minimum Distance
-# Stage 2: From the nums of Direction decide Destination
-# Stage 3: Modify the Dino walking_speed and Create movement
-# Add Cute mode and Devi mode (the dino will randomly drag the user cursor or drag sarcastic window LOL)
+from data import *
 
 window = Tk()
 window.title("Funny Dino")
-window_gap = 50
 
-# Path
-type_of_Dino = 'Blue'
-dino_path = f'sprites/{type_of_Dino}/dino.png'
-idle_path = f'sprites/{type_of_Dino}/Idle'
-walking_path = f'sprites/{type_of_Dino}/Walking'
-running_path = f'sprites/{type_of_Dino}/Running'
-
-# Sprite
-duplicated_sprites = []
-dino_is_flipped = False
-
-idle_index = 0
-idle_time = 600
-idle_sprites = [f'{idle_path}/idle1.png',f'{idle_path}/idle2.png',f'{idle_path}/idle3.png',f'{idle_path}/idle4.png']
-startOf_idleDuplication = True
-
-walking_index = 0
-walking_sprites = [f'{walking_path}/walking1.png',f'{walking_path}/walking2.png',f'{walking_path}/walking3.png',f'{walking_path}/walking4.png',
-					f'{walking_path}/walking5.png',f'{walking_path}/walking6.png']
-
-running_index = 0
-running_sprites = [f'{running_path}/running1.png',f'{running_path}/running2.png',f'{running_path}/running3.png',f'{running_path}/running4.png',
-					f'{running_path}/running5.png',f'{running_path}/running6.png', f'{running_path}/running7.png']
+dino_mode = 'Devi' # Cute or Devi # Cute mode may distribute to cute wanderer or cute with actions
 
 # Child Window
-idtitle = -1
-trolling_images = [] # mostly meme :)) # This actually in the dict with title in a list
-trolling_titles = ['Your mom', 'CDs... CEE DEEZ NUT', 'The diffence between you and the door', 'You monkey']
-trollWindow_positions = [[], []]
-
-# Velocity of the Dino
-walking_speed = 50 # in millisecond
-running_speed = 25
-walking_step = 2
-running_step = 4
+childWindow_width_limitation = window.winfo_screenwidth()/4
+trolling_windows = [ # mostly meme :)) # This actually in the dict with title in a list
+	{
+		"title": 'DARE DEVILLL',
+		"image_path": f'{imagesMeme_path}/threat.jpg'
+	},
+	{
+		"title": 'Uhmm... Terrorist Jutsu',
+		"image_path": f'{imagesMeme_path}/holy_damn.png'
+	},
+	{
+		"title": 'MEOW MEOW, THE CAT CAN MEWWW',
+		"image_path": f'{imagesMeme_path}/mewing_cat.jpeg'
+	},
+	{
+		"title": 'GYATT Trump Lecture',
+		"image_path": f'{imagesMeme_path}/trump_mewing.jpg'
+	},
+	{
+		"title": 'Your Bro\'s Face when hEAr YoUr jOkes',
+		"image_path": f'{imagesMeme_path}/funny_dog.png'
+	}
+	# 'Your mom', 'CDs... CEE DEEZ NUT', 'The diffence between you and the door', 'You monkey'
+]
 
 # Value for Dino Size and Screen Resolution
 sample = PhotoImage(file=dino_path)
@@ -62,17 +45,20 @@ scale = desire_size/sample.width()
 img_width = int(sample.width() * scale)
 img_height = int(sample.height() * scale)
 
-initial_destination = [int(window.winfo_screenwidth()/2 - img_width - 200), int(window.winfo_screenheight()/2 - img_height - 100)]
-
 x = -img_width
 y = 100
 
+initial_destination = [int(window.winfo_screenwidth()/2 - img_width - 200), int(window.winfo_screenheight()/2 - img_height - 100)]
+
+
+# Configure Main Window (The Dino duh)
 window.geometry(f"{img_width}x{img_height}+{x}+{y}")
 window.resizable(False, False)
 window.attributes('-transparentcolor', 'black')
 window.wm_attributes("-topmost", True)
 window.configure(background='black')
 window.overrideredirect(True)
+
 
 def check_close_signal():
 	while True:
@@ -82,34 +68,41 @@ def check_close_signal():
 			break
 		time.sleep(0.5)  # Check every second
 
+
 def destroy_window():
 	window.destroy()
+
 
 def address_deletedWindow(title, troll_window):
 	print(f'This \'{title}\' window is closed')
 	print('And the Dino gonna chase you!')
 	troll_window.destroy()# What if the user close too many times?
 
-def launch_trolling_window(width, height, x, y):
 
-    child_window = Toplevel(window)
+def launch_trolling_window(title, image, x, y):
 
-    random_trollingTitles = random.choice(trolling_titles)
+	child_window = Toplevel(window)
 
 	# will be working with the child window position and there will randomly be a big window in the mid LOL
-    child_window.title(random_trollingTitles)
-    child_window.geometry(f"{width}x{height}+{int(x)}+{int(y)}")
-    child_window.resizable(False, False)
-    child_window.wm_attributes("-topmost", True)
+	child_window.title(title)
+	child_window.resizable(False, False)
+	child_window.wm_attributes("-topmost", True)
 
-    child_window.protocol("WM_DELETE_WINDOW", 
-    	lambda: address_deletedWindow(random_trollingTitles, child_window)
-    )
+	# Create a label to display the image
+	label = Label(child_window, image=image)
+	label.image = image  # Keep a reference to avoid garbage collection
+	label.pack()  # Pack the label to add it to the window
+	
+	child_window.geometry(f"+{int(x)}+{int(y)}")
 
-    return child_window
+	child_window.protocol("WM_DELETE_WINDOW", 
+		lambda: address_deletedWindow(title, child_window)
+	)
+
+	return child_window
 
 
-def resizing_image(path):
+def resizing_image_for_dino(path):
 
 	global dino
 
@@ -144,7 +137,7 @@ def idle(coordination, destination):
 		duplicated_sprites = idle_sprites * random.randint(3, 6)
 		startOf_idleDuplication = False
 
-	idle_image = resizing_image(duplicated_sprites[idle_index])
+	idle_image = resizing_image_for_dino(duplicated_sprites[idle_index])
 
 	label.config(image=idle_image)
 
@@ -158,19 +151,38 @@ def idle(coordination, destination):
 		duplicated_sprites = []
 		choosing_action = random.randint(1,3)
 
-		if choosing_action == 1:
-			child_window_width = 400 # replace the number by the image size
-			child_window_height = 400
-			child_window_gap = 50
+		print(choosing_action)
 
-			child_window_x = random.choice([-width + window_gap, window.winfo_screenwidth() - window_gap])
-			child_window_y = random.choice([child_window_gap, window.winfo_screenheight()-height-child_window_gap])
+		if choosing_action == 1 and dino_mode == 'Devi': # Only be activated in Devi mode
+
+			random_trollingWindow = random.choice(trolling_windows)
+			print('The choosed Image: ', random_trollingWindow)
+			
+			meme_img = Image.open(random_trollingWindow['image_path'])  # Update with your image path
+			print('Meme image Size: ', meme_img.size)
+
+			child_window_width, child_window_height = meme_img.size
+
+			# check image size
+			print('Compare height and Limit:', child_window_height, childWindow_width_limitation)
+			if child_window_height > childWindow_width_limitation:
+				print('Window is resizing')
+				child_window_scale = childWindow_width_limitation/child_window_width
+				child_window_width = childWindow_width_limitation
+				child_window_height = child_window_height*child_window_scale
+				meme_img = meme_img.resize((int(child_window_width), int(child_window_height)))
+
+			memeFR = ImageTk.PhotoImage(meme_img)
+
+			child_window_x = random.choice([-child_window_width + 300, window.winfo_screenwidth() - 300])
+			child_window_y = random.choice([window_gap, window.winfo_screenheight() - child_window_height - window_gap])
 
 			drag_destination = [child_window_x, child_window_y + child_window_height/2 - img_height/2]
-			troll_window = launch_trolling_window(child_window_width, child_window_height, child_window_x, child_window_y)
+			troll_window = launch_trolling_window(random_trollingWindow['title'], memeFR, child_window_x, child_window_y)
 
-			window.after(running_speed, drag_window, coordination, drag_destination, troll_window)
-			# window.after(walking_speed, move, coordination, destination)
+			# window.after(running_speed, drag_window, coordination, drag_destination, troll_window)
+			window.after(walking_speed, move, coordination, destination)
+
 		else:
 			window.after(walking_speed, move, coordination, destination)
 
@@ -179,11 +191,13 @@ def idle(coordination, destination):
 	# continue idle if havent done animation
 	window.after(idle_time, idle, coordination, destination)
 
+
 def drag_window(coordination, destination):
 	# will have first destination with normal running
 	# and second destination with reverse running
 	# if second destination reached move to idle
 	pass
+
 
 def move(coordination, destination): #coordination, destination
 
@@ -203,7 +217,7 @@ def move(coordination, destination): #coordination, destination
 	# print("---------------------------")
 
 	# animation and walking
-	walking_image = resizing_image(walking_sprites[walking_index])
+	walking_image = resizing_image_for_dino(walking_sprites[walking_index])
 
 	if x_relate > 0:
 		dino_is_flipped = True
@@ -250,7 +264,7 @@ def move(coordination, destination): #coordination, destination
 
 threading.Thread(target=check_close_signal, daemon=True).start()
 
-dino = resizing_image(dino_path)
+dino = resizing_image_for_dino(dino_path)
 
 keyboard.add_hotkey("shift+6", destroy_window)
 
@@ -264,7 +278,7 @@ window.mainloop()
  
 # # Show the window
 # def show():
-#     second.deiconify()
+# second.deiconify()
  
 # # Hide the window
 # def hide():
@@ -280,3 +294,18 @@ window.mainloop()
 # smirking[evil] version (occurs when it poop or drag a sarcastic note) and falling baby
 # cute dino must have sound lol
 # must have random idle
+
+# Note: must 4 directions, separate to 2 groups: West & East and South & North || Create Destination (!= current dino coordination); 
+# Must Reckon Limitation First (with the prerequisite: 300 pixels away dino and 500 pixels move maximum 
+# -> Must go to situation that have to determine the directions first bcuz
+# the dino may be close to the computer screen side)
+# Stage 1: Must Locate the Dino first -> Decide the direction || random the direction via Limitation and Minimum Distance
+# Stage 2: From the nums of Direction decide Destination
+# Stage 3: Modify the Dino walking_speed and Create movement
+# Add Cute mode and Devi mode (the dino will randomly drag the user cursor or drag sarcastic window LOL)
+
+# There should be a random here to decide to use the big window in the mid or small window # kinda time consuming so yeah
+
+# Future problems
+# when the user close the window while dino dragging, will the program crash?
+# or when the user close the window will the dino crash when even in idle mode or walking? Cuz it might interrupt the window.after func
